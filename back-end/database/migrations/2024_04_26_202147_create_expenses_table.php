161baecb75ff31be2934130491bdd74bfc68cbd6
anas sawalha <anas.swalha@yahoo.com>
2024-04-26 23:26:18 +0300

    t

---
+++ back-end/database/migrations/2024_04_26_202147_create_expenses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('userId');
+            $table->foreign('userId')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('categoryId');
+            $table->foreign('categoryId')->references('id')->on('expense_categories')->onDelete('cascade');
+            $table->decimal('amount', 10, 2);
+            $table->string('description')->nullable();
+            $table->date('transaction_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
