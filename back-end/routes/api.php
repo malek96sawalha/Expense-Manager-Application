a97d1c8227d50bb5c814108d25a3fee4aff6bf22
Yousef-M-Jaradat <yousef.jaradat87@gmail.com>
2024-04-27 02:01:11 +0300

    yousef

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -23,9 +23,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
- Route::apiResource('incomes', IncomeController::class);
+Route::apiResource('incomes', IncomeController::class);
 
 Route::apiResource('categories', CategorieController::class);
-// Route::get('/categories', [CategorieController::class, 'index']);
+Route::get('category/state-and-user', [CategorieController::class, 'getByStateAndUserId']);
+
 
 Route::apiResource('expense', ExpenseController::class);
