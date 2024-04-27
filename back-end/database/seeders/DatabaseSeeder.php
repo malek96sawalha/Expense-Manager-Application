5197269c730974323a65cd981b0e6917cbeb5b15
anas sawalha <anas.swalha@yahoo.com>
2024-04-27 16:03:57 +0300

    m

--- back-end/database/seeders/DatabaseSeeder.php
+++ back-end/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserSeeder::class);
         $this->call(CategoriesSeeder::class);
-         $this->call(TransactionSeeder::class);
+        $this->call(TransactionSeeder::class);
     }
 }
